0e2edcc62f82d94cce1e5e2710cd11a774762814
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-09 17:39:27 -0300

    Create routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 
+import { bookRouter } from "./routes/book.routes";
+import { authorRouter } from "./routes/author.routes";
+import { userRouter } from "./routes/user.routes";
+import { tagRouter } from "./routes/tag.routes";
+import { collectionRouter } from "./routes/collection.routes";
+
 const app = express();
 
 app.use(cors());
@@ -8,3 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(4000);
+app.use("/api/v1/book", bookRouter);
+app.use("/api/v1/author", authorRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/tag", tagRouter);
+app.use("/api/v1/collection", collectionRouter);

ddc9a6e0a9d54e91f34df8b219be7143912de48d
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-11 15:56:45 -0300

    Add ResponseHandler class

---
+++ src/utils/ResponseHandler.ts
@@ -0,0 +1,9 @@
+import { Response } from "express";
+
+class ResponseHandler {
+    public handleResponse(res: Response, status: number, message: string) {
+        res.status(status).json({ message });
+    }
+}
+
+export default new ResponseHandler();

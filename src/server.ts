1999016861142f27cf397ab7b9660a24025174ca
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-12 17:37:47 -0300

    Add missing connection to the database

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,19 @@ import { authorRouter } from "./routes/author.routes";
 import { userRouter } from "./routes/user.routes";
 import { tagRouter } from "./routes/tag.routes";
 import { collectionRouter } from "./routes/collection.routes";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect(
+    "mongodb+srv://lucasAdmin:lucas1353@cluster0.oximzo5.mongodb.net/project_bookwise?retryWrites=true&w=majority"
+);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(4000);
+app.listen(6000);
 app.use("/api/v1/book", bookRouter);
 app.use("/api/v1/author", authorRouter);
 app.use("/api/v1/user", userRouter);

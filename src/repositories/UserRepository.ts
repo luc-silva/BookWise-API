f96e6534b7ef379724d692bb04a4990233439c58
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-15 17:38:40 -0300

    Add user specific query methods

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -3,8 +3,16 @@ import User from "../model/User";
 import { Repository } from "./Repository";
 
 class UserRepository extends Repository {
-    constructor(user:Model<any>){
-        super(user)
+    constructor(user: Model<any>) {
+        super(user);
+    }
+
+    public async findUserByEmail(email: string) {
+        return await User.findOne({ email });
+    }
+
+    public async findUserById(userId:string){
+        return await this.getItemDetails(userId)
     }
 }
 

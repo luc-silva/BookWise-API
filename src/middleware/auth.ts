5db38462d2776e5139c87b5021ecbd23794b1a83
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-16 17:23:52 -0300

    Fix auth request user

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
 import ResponseHandler from "../utils/ResponseHandler";
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 import UserRepository from "../repositories/UserRepository";
+
+interface Decoded {
+    id: string;
+}
 
 export const protectedRoute = expressAsyncHandler(
     async (request: Request, response: Response, next: NextFunction) => {
@@ -18,12 +22,31 @@ export const protectedRoute = expressAsyncHandler(
             throw new Error("Não Autorizado. Sem Token.");
         }
 
-        let token = request.headers.authorization.split(" ")[1]
-        let decoded = jwt.verify(token, process.env.JWT_SECRET)
-        if(!decoded){
-            ResponseHandler.handleResponse(response, 401, "Não Autorizado. Sem Token.")
-            throw new Error("Não Autorizado. Sem Token.")
+        let token = request.headers.authorization.split(" ")[1];
+        let decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            ResponseHandler.handleResponse(
+                response,
+                401,
+                "Não Autorizado. Sem Token."
+            );
+            throw new Error("Não Autorizado. Sem Token.");
         }
+
+        let decodedUserId = typeof decoded === "string" ? decoded : decoded.id;
+        let user = await UserRepository.findUserById(decodedUserId);
+        if (!user) {
+            ResponseHandler.handleResponse(
+                response,
+                404,
+                "Autenticação Mal Sucedida. Usuário Não Encontrado"
+            );
+            throw new Error(
+                "Autenticação Mal Sucedida. Usuário Não Encontrado"
+            );
+        }
+
+        request.user = user.id;
 
         next();
     }

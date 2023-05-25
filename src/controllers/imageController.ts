44423ce54052fa7f9e0322938c2405651b950570
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-25 20:47:04 -0300

    Add getImage endpoint and JSDoc

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -4,6 +4,11 @@ import ResponseHandler from "../utils/ResponseHandler";
 import ImageRepository from "../repositories/ImageRepository";
 import UserRepository from "../repositories/UserRepository";
 
+/**
+ * PATCH - Update a image with given id and data.
+ * @param request - HTTP request object containing the image id and data object.
+ * @param response - HTTP response object containing the conclusion message.
+ */
 export const updateImage = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.user || !request.file || !request.params) {
@@ -11,7 +16,7 @@ export const updateImage = asyncHandler(
             throw new Error("Dados Inválidos.");
         }
         let { id } = request.params;
-        let { buffer } = request.file
+        let { buffer } = request.file;
 
         let user = await UserRepository.findUserById(request.user);
         if (!user) {
@@ -38,10 +43,31 @@ export const updateImage = asyncHandler(
             throw new Error("Não Autorizado.");
         }
 
-        console.log(buffer)
-
-        //await ImageRepository.updateImage(id, buffer)
+        await ImageRepository.updateImage(id, buffer);
 
         response.status(200).json({ message: "Feito." });
+    }
+);
+
+/**
+ * GET - Get a image instance with given id.
+ * @param request - HTTP request object containing the book id.
+ * @param response - HTTP response object containing the image buffer.
+ */
+export const getImage = asyncHandler(
+    async (request: Request, response: Response) => {
+        if (!request.user || !request.params) {
+            ResponseHandler.handleResponse(response, 400, "Dados Inválidos.");
+            throw new Error("Dados Inválidos.");
+        }
+        let { id } = request.params;
+
+        let image = await ImageRepository.getImageById(id);
+        if(!image){
+            ResponseHandler.handleResponse(response, 404, "Imagem não encontrada.");
+            throw new Error("Imagem não encontrada.");
+        }
+
+        response.status(200).json(image);
     }
 );

f01b698c0dd22d2d016676328017d504eb8a08d7
Lucas Santos <lucas.silva1203@outlook.com>
2023-10-19 16:38:33 -0300

    refactor: replace mongoose with prisma

--- src/model/Collection.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const CollectionSchema = new Schema({
-    collection_name: { type: String, required: true },
-},
-{ timestamps: true });
-
-export = model("Collection", CollectionSchema)
